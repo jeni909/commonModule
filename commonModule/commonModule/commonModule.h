a9119b396d85593524e6a1ffc621e67202728e35
jeni Kakadiya <jeni.arvaan@gmail.com>
2025-02-11 17:03:37 +0530

    init commit

---
+++ commonModule/commonModule/commonModule.h
@@ -0,0 +1,18 @@
+//
+//  commonModule.h
+//  commonModule
+//
+//  Created by Arvaan Jeni on 11/02/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for commonModule.
+FOUNDATION_EXPORT double commonModuleVersionNumber;
+
+//! Project version string for commonModule.
+FOUNDATION_EXPORT const unsigned char commonModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <commonModule/PublicHeader.h>
+
+
---
+++ commonModule/commonModule/commonModule/commonModuleVC.swift
@@ -0,0 +1,29 @@
+//
+//  commonModuleVC.swift
+//  commonModule
+//
+//  Created by Arvaan Jeni on 11/02/25.
+//
+
+import UIKit
+
+class commonModuleVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
